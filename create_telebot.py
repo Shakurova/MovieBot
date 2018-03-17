e90540edd05c1c5e933937a388ba6c9fd16c1217
Lena <lenashakurova@yandex.ru>
2018-03-17 21:05:33 +0100

    crawler and amazon parser

--- create_telebot.py
+++ create_telebot.py
@@ -1,13 +1,51 @@
 # -*- coding: utf-8 -*-
 
 import telebot
+import tmdbsimple as tmdb
+import ujson
 
 bot = telebot.TeleBot('564436346:AAHNnOHgYONKDixXfRVg8j3pGdNFoaf9IA4')
+tmdb.API_KEY = '63d059aa35be3c4f80368d5a192cfe26'
+
+movie = tmdb.Movies(603)
+response = movie.info()
+print(response['overview'])
+print(movie.title)
+print(movie.budget)
+print(movie.reviews()['results'][0])
+print(len(movie.reviews()['results']))
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+print('Recommendations', movie.recommendations())
 
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
+with open('recommendation.json', 'w') as w:
+    ujson.dump(movie.recommendations(), w)
+
+
+response = movie.releases()
+for c in movie.countries:
+   if c['iso_3166_1'] == 'US':
+        print(c['certification'])
+
+search = tmdb.Search()
+response = search.movie(query='The Bourne')
+for s in search.results:
+    print(s['title'], s['id'], s['release_date'], s['popularity'])
+
+
+person = tmdb.People(1)
+print(person.info())
+
+
+
+# similar_movies
+# releases
+#
+# recommendations
+
+# @bot.message_handler(content_types=["text"])
+# def repeat_all_messages(message):
+#     bot.send_message(message.chat.id, message.text)
+#
+# if __name__ == '__main__':
+#      bot.polling(none_stop=True)
---
+++ scripts.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+import ujson
+
+dictionary = {}
+
+
+def create_dict(arr):
+    global dictionary
+    # print('!!!!!!!!', len(arr))
+    # print('????', len(dictionary.keys()))
+    for i in '#'.join(arr).split('BREAK'):
+        a = [a for a in i.split('#') if a != '']
+        if len(a) > 3:
+            name = a[0].replace("'", "")
+            # score = a[2].replace("'", "")
+            # helpfulness = a[1].replace("'", "")
+            if name not in dictionary:
+                dictionary[name] = {}
+                dictionary[name]['review'] = []
+                dictionary[name]['review'].append((a[1], a[2], a[3]))
+            else:
+                dictionary[name]['review'].append((a[1], a[2], a[3]))
+
+    # # for i in dictionary:
+    # #     print(i, dictionary[i])
+
+    with open('amazon2.json', 'w') as w:
+        ujson.dump(dictionary, w)
+    # return dictionary
+
+
+def amazon_parser(filename='amazon_movies.txt'):
+
+    with open(filename, 'rb') as f:
+        count = 0
+        arr = []
+
+        for line in f:
+            # print(line)
+            # line = str(line, 'utf-8')
+            count += 1
+            if count < 80000000:
+                # if count / 10 == 0:
+                #     print(count)
+                # print(str(line).split(':')[0])
+                # print(str(line).split(':')[0][2::])
+                if line != b"\n":
+                    if str(line).split(':')[0][2::] == 'product/productId':
+                        arr.append(str(line).replace('\\n', '').split(': ')[1])
+                    if str(line).split(':')[0][2::] == 'review/text':
+                        arr.append(str(line).replace('\\n', '').split('review/text: ')[1])
+                    if str(line).split(':')[0][2::] == 'review/helpfulness':
+                        arr.append(str(line).replace('\\n', '').split(': ')[1])
+                    if str(line).split(':')[0][2::] == 'review/score':
+                        arr.append(str(line).replace('\\n', '').split(': ')[1])
+
+                elif line == b"\n" or len(line) < 3:
+                    arr.append('BREAK')
+                    if count % 1000 == 0:
+                        print(count)
+                        # print('here')
+                        create_dict(arr)
+            else:
+                break
+
+    return arr
+
+
+if __name__ == '__main__':
+    # arr = amazon_parser()
+
+    # Transform crawled data into a normal format
+    results = ujson.load(open('results2.json'))
+
+    lookup_dictionary = {}
+    for l in results:
+        lookup_dictionary[l['event_url'].split('/')[-1]] = l['event_title'][0].split(': ')[2]
+
+    with open('lookup_dictionary2.json', 'w') as w:
+        ujson.dump(lookup_dictionary, w)
+
+    print('start with amazon')
+    # transform amazon keys
+    amazon = ujson.load(open('amazon2.json'))
+    print(len(amazon.keys()))
+
+    nice_amazon = {}
+
+    for k in amazon:
+        if k in lookup_dictionary:
+            nice_amazon[lookup_dictionary[k]] = amazon[k]
+        else:
+            print(k)
+
+    with open('nice_amazon2.json', 'w') as w:
+        ujson.dump(nice_amazon, w)
+
+    nice_amazon2 = ujson.load(open('nice_amazon2.json'))
+    print(len(nice_amazon2.keys()))
+
+    for k in nice_amazon2:
+        print(k)
+
