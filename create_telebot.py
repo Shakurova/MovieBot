f6b6985ad4e24b075917adb3b806f2c6c1c75931
Lena <lenashakurova@yandex.ru>
2018-03-27 18:28:11 +0200

    c

--- create_telebot.py
+++ create_telebot.py
@@ -17,33 +17,38 @@ NYT_URL = '/reviews/search.json'
 def is_greeting(message):
     return matches(message, phrases.their_greetings)
 
+
 def is_goodbye(message):
     return matches(message, phrases.their_goodbyes)
 
+
 def is_ask_score(message):
     return matches(message, phrases.their_ask_score)
+
 
 # TODO - bug: "w*hi*ch grade are you in?" matches "hi"
 def matches(message, messages):
     return any(True for g in messages if message.text.lower() in g.lower() or g.lower() in message.text.lower())
 
+
 @bot.message_handler(func=is_greeting, content_types=['text'])
 def greeting(message):
     bot.send_message(message.chat.id, random.choice(phrases.my_greetings))
+
 
 @bot.message_handler(func=is_goodbye, content_types=['text'])
 def goodbye(message):
     bot.send_message(message.chat.id, random.choice(phrases.my_goodbyes))
 
+
 @bot.message_handler(func=is_ask_score, content_types=['text'])
 def answer_score(message):
     bot.send_message(message.chat.id, "The average score for that movie is {}".format(random.randint(1, 5)))
 
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, random.choice(phrases.my_confusion))
-
-
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
